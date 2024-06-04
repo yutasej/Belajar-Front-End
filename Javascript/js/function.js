179ac31ae16651d24c5e09bf7a62f65941dead04
yutasej <128043966+yutasej@users.noreply.github.com>
2024-06-04 21:42:13 +0700

    refactoring function.js

--- Javascript/js/function.js
+++ Javascript/js/function.js
@@ -1,41 +1,34 @@
 alert("Kalkulator Sederhana \n1. Tambah 2. Kurang 3. Kali 4. Bagi");
 
 function tambah(a, b) {
-  var total = a + b;
-
-  return total;
+  return a + b;
 }
 
 function kurang(a, b) {
-  var total = a - b;
-
-  return total;
+  return a - b;
 }
 
 function kali(a, b) {
-  var total = a * b;
-
-  return total;
+  return a * b;
 }
 
 function bagi(a, b) {
-  var total = a / b;
-
-  return total;
+  return a / b;
 }
 
-var lagi = true;
+let lagi = true;
 while (lagi) {
-  var pilih = prompt("Pilih Operasi Perhitungan! (1-4)");
-  if (pilih == 1) {
+  var pilih = parseInt(prompt("Pilih Operasi Perhitungan! (1-4)"));
+
+  if (pilih === 1) {
     var a = parseInt(prompt("Masukkan Angka Pertama:"));
     var b = parseInt(prompt("Masukkan Angka Kedua:"));
     alert("Hasilnya Adalah " + tambah(a, b));
-  } else if (pilih == 2) {
+  } else if (pilih === 2) {
     var a = parseInt(prompt("Masukkan Angka Pertama:"));
     var b = parseInt(prompt("Masukkan Angka Kedua:"));
     alert("Hasilnya Adalah " + kurang(a, b));
-  } else if (pilih == 3) {
+  } else if (pilih === 3) {
     var a = parseInt(prompt("Masukkan Angka Pertama:"));
     var b = parseInt(prompt("Masukkan Angka Kedua:"));
     alert("Hasilnya Adalah " + kali(a, b));
@@ -46,5 +39,7 @@ while (lagi) {
   } else {
     alert("Masukkan Operasi Perhitungan Yang Benar!");
   }
+
   lagi = confirm("Lagi ?");
 }
+
